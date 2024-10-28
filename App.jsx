c78ba09e2ba835de58caf706c7b62cb882ddc48a
alexsia08 <156172931+alexsia08@users.noreply.github.com>
2024-10-29 01:32:56 +0700

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,32 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { BodyFon } from "./body/bodyFon";
-import { CSSAppContainer, CSSAppBulls, CSSAppCows, CSSAppHeader, CSSAppNumberRead, CSSAppString, CSSAppNumpad } from './styles/styles';
+import { CSSAppContainer, CSSAppBulls, CSSAppCows, CSSAppHeader, CSSAppNumberRead, CSSAppString, CSSAppNumpad, CSSCircle, CSSCircleOne } from './styles/styles';
 import { NumberPad } from "./numpad/NumberPad";
+import { CSSEnter, CSSRestat, CSSBackSpace} from "./numpad/styles";
+
 
 export const App = () => {
-  // const numberList = [
-  //   {
-  //     number:1,
-  //     onClick:() => {}
-  //   },
-  //   {
-  //     number:2,
-  //     onClick:() => {}
-  //   },
-  // ]
+  const [version, setVersion] = useState(0);
+  function handelReset() {
+    setVersion(version + 1);
+    setNumber("");
+  }
 
+  const [number, setNumber] = useState("");
+  const onAddNumber = (digit) => {
+    if(number.length < 4) {
+      setNumber((prev) => prev + digit);
+    }else{
+      alert("Максимум 4 цифры!");
+    }
+  }
+
+  const handleBackSpace = () => {
+    setNumber((prev) => prev.slice(-1, -1));
+  }
+
+  const [InputValue, setInputValue] = useState('');
+  const onAddEnter = () => {
+    alert(`Вы ввели: ${number}`);
+    setNumber('');
+  }
 
   return <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '97vh' }}> 
       <BodyFon />
       <CSSAppContainer>
-        <CSSAppHeader></CSSAppHeader>
+        <div>
+          <CSSAppHeader>
+            <CSSRestat onClick={handelReset}>{String.fromCodePoint(0x27F3)}</CSSRestat>
+            <CSSCircle />
+            <CSSCircleOne />
+          </CSSAppHeader>
+        </div>
+
 
         <div style={{display:"flex", justifyContent:"space-between"}}>
           <CSSAppNumpad>
-            <CSSAppString></CSSAppString>
-            {
-              <NumberPad />
-            }
+            <CSSAppString>{number}</CSSAppString>
+            <NumberPad onAddNumber={onAddNumber}/>
+            <CSSBackSpace onClick={handleBackSpace}>Стереть</CSSBackSpace> 
+            <CSSEnter onClick={onAddEnter}>Ввести</CSSEnter>  
           </CSSAppNumpad>
 
           <CSSAppNumberRead>ЗДЕСЬ БУДУТ ЦИФРЫ, КОТОРЫЕ ВЫ ВВЕЛИ РАНЕЕ!</CSSAppNumberRead>
